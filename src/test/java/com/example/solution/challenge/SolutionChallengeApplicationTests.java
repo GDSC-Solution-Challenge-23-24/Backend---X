3c89623d5ba2a109de09854bb2a481ffbaf1ef9a
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-01-08 18:45:04 +0900

    joon ] 스프링 초기 설정

---
+++ src/test/java/com/example/solution/challenge/SolutionChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solution.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
